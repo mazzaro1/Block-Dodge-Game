43b68ecca3cdf7c283a433410064ef51e6befbbb
mazzaro1 <gustamazzaro@gmail.com>
2020-03-11 15:35:13 -0300

    Scripts do jogo
    Scripts do jogo

--- Assets/Scripts/RotacionarBolas.cs
+++ Assets/Scripts/RotacionarBolas.cs
@@ -1,18 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RotacionarBolas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //velocidade do movimento
+    public float velocidade;
+    //controle da direcao
+    bool direita = false;
+
+    //objeto que e o gameover
+    public GameObject gameOver;
+    //sua pontuacao
+    public int pontuacao;
+
+    //objeto text que referencia o texto
+    public Text pontuacaoTexto;
+
+
+    void Update()
     {
+        //se clicar com o botão esquerdo do mouse
+        if(Input.GetMouseButtonDown(0))
+        {
+            //inverte valor do booleno
+            direita = !direita;
+        }
+
+        //se o boleeano for falso
+        if(direita == false)
+        {
+            //se a condicao for atendida, executa isso aqui
+            transform.Rotate(new Vector3(0f, 0f, velocidade * Time.deltaTime));
+        }
+        else
+        {
+            //se a condicao nao for atendida, executa isso aqui
+            transform.Rotate(new Vector3(0f, 0f, -velocidade * Time.deltaTime));
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //metodo que ativa o objeto do gameover
+    public void ChamarGameOver()
     {
-        
+        gameOver.SetActive(true);
     }
+
+    //metodo que recarrega a cena
+    public void ReiniciarGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //metodo que atualiza a pontuacao
+    public void AtualizarPontuacao()
+    {
+        pontuacao++;
+        pontuacaoTexto.text = pontuacao.ToString(); 
+    }
+
 }
